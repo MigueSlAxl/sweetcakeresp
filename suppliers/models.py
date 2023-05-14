a24302bd18682fdc5fe2a5abbcbd25103d607509
Nikolo <sanchez.nicolas.9917@gmail.com>
2023-05-13 22:31:05 -0400

    arreglo suppliers imagen

--- suppliers/models.py
+++ suppliers/models.py
@@ -1,12 +1,31 @@
 from django.db import models
+import base64
+from django.core.files import File
+import os
+from django.conf import settings
 
 # Create your models here.
 class Supplier(models.Model):
     rut=models.CharField(max_length=150,blank=False,null=False);
     nombre_proveedor=models.CharField(max_length=150,blank=False,null=False)
     tipo_insumo=models.CharField(max_length=150,blank=False,null=False)
+    imagen_insumo=models.ImageField(upload_to='insumo/',blank=False,null=True)
     correo_proveedor=models.EmailField(max_length=150,blank=False,null=False)
     telefono_proveedor=models.IntegerField(blank=False,null=False)
+    def imagen_base64(self):
+        if self.imagen_insumo and hasattr(self.imagen_insumo, 'url'):
+            with self.imagen_insumo.open(mode='rb') as f:
+                img_data = f.read()
+            return base64.b64encode(img_data).decode('utf-8')
+        else:
+            return None
+    def save(self, *args, **kwargs):
+        if not self.imagen_insumo:
+            # asigna la imagen por defecto si no se ha proporcionado una imagen
+            img_path = os.path.join(settings.MEDIA_ROOT, 'insumo/default.jpg')
+            with open(img_path, 'rb') as f:
+                self.imagen_insumo.save('insumo/default.jpg', File(f), save=False)
+        super(Supplier, self).save(*args, **kwargs)
     class Meta:
         ordering = ['id']
     def __str__(self):
--- suppliers/views.py
+++ suppliers/views.py
@@ -1,3 +1,5 @@
+import base64
+import os
 from django.shortcuts import render
 from rest_framework.decorators import (api_view, authentication_classes, permission_classes)
 from .models import Supplier
@@ -7,12 +9,17 @@ from rest_framework.decorators import api_view, authentication_classes, permissi
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics, viewsets, serializers,status
+from drf_extra_fields.fields import Base64ImageField
+from django.conf import settings
+from PIL import Image
+from django.core.files.base import ContentFile
 # Create your views here.
 
 class SupplierSerializadorImagenJson(serializers.ModelSerializer):
+    imagen_insumo=Base64ImageField(required=False)
     class Meta:
         model=Supplier
-        fields=['id','nombre_proveedor','rut','tipo_insumo','correo_proveedor','telefono_proveedor']
+        fields=['id','nombre_proveedor','rut','tipo_insumo','correo_proveedor','telefono_proveedor','imagen_insumo']
 
 @api_view(['POST'])
 def suppliers_suppliers_add_rest(request, format=None):
@@ -24,15 +31,21 @@ def suppliers_suppliers_add_rest(request, format=None):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BADsuppliers)
     
+class SupplierSerializer(serializers.ModelSerializer):
+    imagen_insumo = serializers.SerializerMethodField()
+    class Meta:
+        model = Supplier
+        fields = ('id','nombre_proveedor','rut','tipo_insumo','correo_proveedor','telefono_proveedor','imagen_insumo')
 
+    def get_imagen_insumo(self, obj):
+        return obj.imagen_base64()
+    
 @api_view(['GET'])
 def suppliers_suppliers_list_rest(request, format=None):
     if request.method == 'GET':
-        suppliers_list = Supplier.objects.all()
-        suppliers_json = []
-        for es in suppliers_list:
-            suppliers_json.append({'id':es.id,'nombre_proveedor':es.nombre_proveedor,'rut':es.rut,'tipo_insumo':es.tipo_insumo,'correo_proveedor':es.correo_proveedor,'telefono_proveedor':es.telefono_proveedor})
-        return Response({'ListSup':suppliers_json})
+        supplier_list = Supplier.objects.all()
+        serializer = SupplierSerializer(supplier_list, many=True)
+        return JsonResponse({'ListSup': serializer.data}, safe=False)
     else:
         return Response({'Msj':"Error método no soportado"})
     
@@ -40,35 +53,53 @@ def suppliers_suppliers_list_rest(request, format=None):
 def suppliers_suppliers_update_rest(request, format=None):
     if request.method == 'POST':
         try:
-            id=request.data['id']
-            nombre_proveedor=request.data['nombre_proveedor']
-            rut=request.data['rut']
-            tipo_insumo=request.data['tipo_insumo']
-            correo_proveedor=request.data['correo_proveedor']
-            telefono_proveedor=request.data['telefono_proveedor']
-            if nombre_proveedor != '':
-                Supplier.objects.filter(pk=id).update(nombre_proveedor=nombre_proveedor)
-                Supplier.objects.filter(pk=id).update(rut=rut)
-                Supplier.objects.filter(pk=id).update(tipo_insumo=tipo_insumo)
-                Supplier.objects.filter(pk=id).update(correo_proveedor=correo_proveedor)
-                Supplier.objects.filter(pk=id).update(telefono_proveedor=telefono_proveedor)
-                suppliers_json=[]
-                suppliers_array = Supplier.objects.get(pk=id)
-                suppliers_json.append({'id':suppliers_array.id,
-                                       'nombre':suppliers_array.nombre_proveedor,
-                                       'rut':suppliers_array.rut,
-                                       'tipo_insumo':suppliers_array.tipo_insumo,
-                                       'correo_proveedor':suppliers_array.correo_proveedor,
-                                       'telefono_proveedor':suppliers_array.telefono_proveedor})
-                return Response({'Msj':"Datos Actualizados",suppliers_array.nombre_proveedor:suppliers_json}) 
+            id=request.data['supplierId']
+            nombre_proveedor= request.data['nombre_proveedor']
+            tipo_insumo = request.data['tipo_insumo']
+            rut = request.data['rut']
+            correo_proveedor = request.data['correo_proveedor']
+            telefono_proveedor = request.data['telefono_proveedor']
+            imagen=request.data.get('imagen_insumo')  # Usar get() para evitar KeyError si la imagen no está presente
+            # Si imagen no tiene ningún valor, establecer imagen_data como None y cargar una imagen predeterminada
+            if imagen:
+                # Procesar la imagen
+                # data = imagen.split(',', 1)[1]  # Remover el prefijo 'data:image/png;base64,'
+                image_data = base64.b64decode(imagen)
+                # image = Image.open(ContentFile(image_data))
+
+                # Guardar la imagen en el modelo de base de datos
+                supplier = Supplier.objects.get(pk=id)
+                supplier.nombre_proveedor = nombre_proveedor
+                supplier.rut = rut
+                supplier.tipo_insumo =tipo_insumo
+                supplier.correo_proveedor= correo_proveedor
+                supplier.telefono_proveedor = telefono_proveedor
+                supplier.imagen_insumo.save(f'{id}.png', ContentFile(image_data), save=True)
             else:
-                return Response({'Msj': "Error los datos no pueden estar en blanco"})
+                # Si no se proporciona una imagen, establecer imagen_data como None y cargar la imagen predeterminada
+                image_path = os.path.join(settings.MEDIA_ROOT, 'insumo/default.jpg')
+                with open(image_path, 'rb') as f:
+                    image_data = f.read()
+
+                # Guardar la imagen predeterminada en el modelo de base de datos
+                supplier = Supplier.objects.get(pk=id)
+                supplier.nombre_proveedor = nombre_proveedor
+                supplier.rut = rut
+                supplier.tipo_insumo =tipo_insumo
+                supplier.correo_proveedor= correo_proveedor
+                supplier.telefono_proveedor = telefono_proveedor
+                supplier.imagen_insumo.save(f'{id}.png', ContentFile(image_data), save=True)
+            
+            # Serializar los datos y enviar la respuesta
+            supplier_array = Supplier.objects.get(pk=id)
+            supplier_json={'id':supplier_array.id,'nombre_proveedor':supplier_array.nombre_proveedor,'rut':supplier_array.rut,'tipo_insumo':supplier_array.tipo_insumo,'correo_proveedor': supplier_array.correo_proveedor,'telefono_proveedor':supplier_array.telefono_proveedor}
+            return Response({'Msj':"Datos Actualizados",supplier_array.nombre_proveedor:[supplier_json]}) 
         except Supplier.DoesNotExist:
             return Response({'Msj':"Error no hay coincidencias"})
         except ValueError:
             return Response({'Msj':"Valor no soportado"})
     else:
-        return Response({'Msj': "Error método no soportado"})
+        return Response({'Msj':"Metodo no soportado"})
         
 @api_view(['POST'])
 def suppliers_suppliers_delete_rest(request, format=None):
