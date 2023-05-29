6008536d62dbea4300dbfd1b270abfe1c0a4298b
Miguel Quiroz <miguel881@live.cl>
2023-05-28 20:43:56 -0400

    c de usuarios finalmente

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+<<<<<<< Updated upstream
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth import get_user_model
+=======
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.shortcuts import get_object_or_404, redirect
+>>>>>>> Stashed changes
 import os
 from django.core.files import File
 import base64
 from django.conf import settings
+<<<<<<< Updated upstream
 # Create your models here.
 
 
@@ -29,6 +36,22 @@ class User(AbstractUser):
         related_name='users'
     )
 
+=======
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    tipo=models.CharField(max_length=150,blank=True,null=True)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, default=1) 
+    token_app_session = models.CharField(max_length = 240,null=True, blank=True, default='')
+    first_session = models.CharField(max_length = 240,null=True, blank=True, default='Si')
+    cargo = models.CharField(max_length=12, blank=True, null=True, verbose_name='cargos')
+    rut = models.CharField(max_length=12, blank=True, null=True, verbose_name='RUT')
+    direccion = models.CharField(max_length=255, blank=True, null=True, verbose_name='direcciones')
+    ntelefono = models.CharField(max_length=15, blank=True, null=True, verbose_name='numero de telefono')
+    nemergencia = models.CharField(max_length=15, blank=True, null=True, verbose_name='numero de emergencia')
+    local = models.CharField(max_length=50, blank=True, null=True, verbose_name='locales')
+    imagen = models.ImageField(upload_to='accounts/', blank=False, null=True)
+>>>>>>> Stashed changes
     def imagen_base64(self):
         if self.imagen and hasattr(self.imagen, 'url'):
             with self.imagen.open(mode='rb') as f:
@@ -36,6 +59,7 @@ class User(AbstractUser):
             return base64.b64encode(img_data).decode('utf-8')
         else:
             return None
+<<<<<<< Updated upstream
 
     def save(self, *args, **kwargs):
         if not self.imagen:
@@ -44,7 +68,16 @@ class User(AbstractUser):
             with open(img_path, 'rb') as f:
                 self.imagen.save('accounts/default1.jpg', File(f), save=False)
         super(User, self).save(*args, **kwargs)
+=======
+>>>>>>> Stashed changes
 
+    def save(self, *args, **kwargs):
+        if not self.imagen:
+            # asigna la imagen por defecto si no se ha proporcionado una imagen
+            img_path = os.path.join(settings.MEDIA_ROOT, 'accounts/default.jpg')
+            with open(img_path, 'rb') as f:
+                self.imagen.save('default.jpg', File(f), save=False)
+        super(Profile, self).save(*args, **kwargs)
     class Meta:
         verbose_name = "user"
         verbose_name_plural = "users"
