9c779c34bfdc4ea847e9e0dedfbd0974047770c0
Nikolo <sanchez.nicolas.9917@gmail.com>
2023-05-30 21:12:28 -0400

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,8 @@ accounts_urlpatterns = [
     path('user_user_add_rest/',views.user_user_add_rest),
     path('user_user_delete_rest/',views.user_user_delete_rest),
     path('user_user_update_rest/',views.user_user_update_rest),
+    path('reset_password/',views.password_reset_email),
+    path('reset_password_confirm/',views.password_reset_confirm),
     # path('signup/', SignUpView.as_view(), name="signup"),
     # path('profile/', ProfileUpdate.as_view(), name="profile"),  
     # path('profile/email/', EmailUpdate.as_view(), name="profile_email"),       
